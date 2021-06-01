197c0a97b7408d5e9b5794deea3273fc85646f23
TomekS98 <43841488+TomekS98@users.noreply.github.com>
2021-06-01 22:51:54 +0200

    [2021-06-01] Simulation app v0.0.1

---
+++ Mockup_Usage/main.py
@@ -0,0 +1,19 @@
+import requests
+import view as v
+from tkinter import *
+import tkinter as tk
+
+root = tk.Tk()
+
+wrapper = LabelFrame(root, text="Symulejszyn")
+wrapper_skilifts = LabelFrame(root, text="Ski Lifts")
+
+v.initialize_view(tk, wrapper, wrapper_skilifts)
+
+root.title("DOSKOZAAAA")
+root.geometry("1024x720")
+root.mainloop()
+
+
+
+
