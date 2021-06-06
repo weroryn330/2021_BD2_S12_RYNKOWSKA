ab525af6eca5f0d6c4eb2c6f86291a13dbedb69c
Szymon Sochaj <szsochaj@gmail.com>
2021-06-06 21:31:22 +0200

    User credentials change functionality added.

--- Angular_Frontend/skiresort/src/app/components/user-edit-credentials/user-edit-credentials.component.ts
+++ Angular_Frontend/skiresort/src/app/components/user-edit-credentials/user-edit-credentials.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {TokenService} from "../../services/token.service";
+import {UserService} from "../../services/user.service";
+import {RegistrationRequest} from "../../classes/registration-request";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-user-edit-credentials',
@@ -7,11 +11,50 @@ import {Component, OnInit} from '@angular/core';
     '../registration/registration.component.css']
 })
 export class UserEditCredentialsComponent implements OnInit {
+  emailForm: any = {
+    email: null,
+    emailRepetition: null
+  }
 
-  constructor() {
+  passwordForm: any = {
+    password: null,
+    passwordRepetition: null
+  }
+  constructor(private token: TokenService, private userService: UserService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
+  onSubmitPassword() {
+    this.userService.changePassword(new RegistrationRequest(this.token.getUser().firstName,
+      this.token.getUser().lastName,this.token.getUser().country, this.token.getUser().email,
+      this.token.getUser().phone, this.token.getUser().voivodeship, this.token.getUser().postalCode,
+      this.token.getUser().city, this.token.getUser().address, this.passwordForm.password)).subscribe(data => {
+        this.token.saveToken(data.token);
+        this.token.saveUser(data);
+        this.router.navigateByUrl('/profile');
+        alert("Hasło zmieniono pomyślnie!");
+      },
+      error => {
+
+        alert("Hasło zmieniono niepomyślne!" + error.error.message);
+      })
+  }
+
+  onSubmitEmail() {
+    this.userService.changeEmail(new RegistrationRequest(this.token.getUser().firstName,
+      this.token.getUser().lastName,this.token.getUser().country, this.emailForm.email,
+      this.token.getUser().phone, this.token.getUser().voivodeship, this.token.getUser().postalCode,
+      this.token.getUser().city, this.token.getUser().address, '')).subscribe(data => {
+        this.token.saveToken(data.token);
+        this.token.saveUser(data);
+        this.router.navigateByUrl('/profile');
+        alert("Email zmieniono pomyślnie!");
+      },
+      error => {
+
+        alert("Email zmieniono niepomyślne!" + error.error.message);
+      })
+  }
 }
---
+++ Angular_Frontend/skiresort/src/app/services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {RegistrationRequest} from "../classes/registration-request";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
+  endPoint = 'http://localhost:8080/api/user'
+  constructor(private http: HttpClient) { }
+
+  changePassword(userData: RegistrationRequest): Observable<any> {
+    return this.http.put<RegistrationRequest>(this.endPoint + '/password',
+      userData ,this.httpOptions)
+  }
+
+  changeEmail(userData: RegistrationRequest): Observable<any> {
+    return this.http.put<RegistrationRequest>(this.endPoint + '/email',
+      userData ,this.httpOptions)
+  }
+}
