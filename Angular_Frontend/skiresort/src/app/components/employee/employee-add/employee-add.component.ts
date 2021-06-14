1481b73033018da59efde1ab1d2704e117f467b2
Szymon Sochaj <szsochaj@gmail.com>
2021-06-15 00:15:50 +0200

    Employee addition form

--- Angular_Frontend/skiresort/src/app/components/employee/employee-add/employee-add.component.ts
+++ Angular_Frontend/skiresort/src/app/components/employee/employee-add/employee-add.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-employee-add',
   templateUrl: './employee-add.component.html',
-  styleUrls: ['./employee-add.component.css']
+  styleUrls: ['./employee-add.component.css',
+    '../../registration/registration.component.css']
 })
 export class EmployeeAddComponent implements OnInit {
+  form: any = {
+    firstName: null,
+    lastName: null,
+    country: null,
+    email: null,
+    emailRepetition: null,
+    phone: null,
+    voivodeship: null,
+    postalCode: null,
+    city: null,
+    address: null,
+    password: null,
+    passwordRepetition: null,
+    employeeType: "ROLE_TECHNICAL_EMPLOYEE"
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    return false;
+  }
+
+  insertDash() {
+
+  }
 }
