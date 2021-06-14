335f61837313754f6637fa140ee47856744a30d8
Marcin Rogoż <marcin.rogoz1@outlook.com>
2021-06-14 21:57:53 +0200

    Owner api

--- Spring_Backend/src/main/java/polsl/tab/skiresort/api/owner/OwnerApi.java
+++ Spring_Backend/src/main/java/polsl/tab/skiresort/api/owner/OwnerApi.java
@@ -1,10 +1,66 @@
 package polsl.tab.skiresort.api.owner;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import polsl.tab.skiresort.api.entry.request.UserRequest;
+import polsl.tab.skiresort.api.entry.response.UserResponse;
+import polsl.tab.skiresort.api.entry.service.RegisterService;
+import polsl.tab.skiresort.api.owner.service.OwnerEmployeeService;
+import polsl.tab.skiresort.api.passes.response.InvoiceResponse;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/owner")
 public class OwnerApi {
-    // TODO Expose api for owner
+
+    private final OwnerEmployeeService ownerEmployeeService;
+
+    private final RegisterService registerService;
+
+    public OwnerApi(
+            OwnerEmployeeService ownerEmployeeService,
+            RegisterService registerService
+    ) {
+        this.ownerEmployeeService = ownerEmployeeService;
+        this.registerService = registerService;
+    }
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<UserResponse>> getAllEmployees() {
+        return ResponseEntity.ok(ownerEmployeeService.getAllEmployees());
+    }
+
+    @GetMapping("/skiers")
+    public ResponseEntity<List<UserResponse>> getAllSkiers() {
+        return ResponseEntity.ok(ownerEmployeeService.getAllSkiers());
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(ownerEmployeeService.getAllUsers());
+    }
+
+    @GetMapping("/invoices/all")
+    public ResponseEntity<List<InvoiceResponse>> getAllInvoices() {
+        return ResponseEntity.ok(ownerEmployeeService.getAllInvoices());
+    }
+
+    @GetMapping("/invoice/{invoiceId}")
+    public ResponseEntity<InvoiceResponse> getInvoiceById(@PathVariable("invoiceId") Integer id) {
+        return ResponseEntity.ok(ownerEmployeeService.getInvoiceById(id));
+    }
+
+    @PutMapping("/editAccount")
+    public ResponseEntity<UserResponse> editEmployeeAccount(@RequestBody UserRequest request) {
+        return ResponseEntity.ok(ownerEmployeeService.editEmployeeAccount(request));
+    }
+
+    @PostMapping("/employees/add")
+    public ResponseEntity<UserResponse> addNewEmployee(
+            @RequestParam("roleName") String roleName,
+            @RequestBody UserRequest request
+    ) {
+        return ResponseEntity.ok(registerService.registerUser(request, roleName));
+    }
 }
---
+++ Spring_Backend/src/main/java/polsl/tab/skiresort/api/owner/service/OwnerEmployeeService.java
@@ -0,0 +1,64 @@
+package polsl.tab.skiresort.api.owner.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import polsl.tab.skiresort.api.entry.request.UserRequest;
+import polsl.tab.skiresort.api.entry.response.UserResponse;
+import polsl.tab.skiresort.api.passes.response.InvoiceResponse;
+import polsl.tab.skiresort.model.User;
+import polsl.tab.skiresort.repository.InvoiceRepository;
+import polsl.tab.skiresort.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class OwnerEmployeeService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final InvoiceRepository invoiceRepository;
+
+    public OwnerEmployeeService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            InvoiceRepository invoiceRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.invoiceRepository = invoiceRepository;
+    }
+
+    public List<UserResponse> getAllSkiers() {
+        return userRepository.findAllSkiers().stream().map(UserResponse::new).collect(Collectors.toList());
+    }
+
+    public List<UserResponse> getAllEmployees() {
+        return userRepository.findAllEmployees().stream().map(UserResponse::new).collect(Collectors.toList());
+    }
+
+    public List<UserResponse> getAllUsers() {
+        return userRepository.findAll().stream().map(UserResponse::new).collect(Collectors.toList());
+    }
+
+    public List<InvoiceResponse> getAllInvoices() {
+        return invoiceRepository.findAll().stream().map(InvoiceResponse::new).collect(Collectors.toList());
+    }
+
+    public InvoiceResponse getInvoiceById(Integer invoiceId) {
+        return new InvoiceResponse(invoiceRepository.findById(invoiceId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Invoice not found")));
+    }
+
+    public UserResponse editEmployeeAccount(UserRequest request) {
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+        return new UserResponse(userRepository.save(User.editMapping(user, request, true)));
+    }
+}
