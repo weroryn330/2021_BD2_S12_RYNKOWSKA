af5910ffb479db0a0a4fd3986837a90f2d27a964
Szymon Sochaj <szsochaj@gmail.com>
2021-06-18 12:43:21 +0200

    Single time pass component.

---
+++ Angular_Frontend/skiresort/src/app/components/employee/pricelist-edit/time-passes-element/element-single-time-pass/element-single-time-pass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-element-single-time-pass',
+  templateUrl: './element-single-time-pass.component.html',
+  styleUrls: ['./element-single-time-pass.component.css']
+})
+export class ElementSingleTimePassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
